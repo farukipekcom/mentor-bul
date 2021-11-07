296537a6580aaaef0c52c7e46292a3877f2a7375
Faruk İpek <farukipekweb@gmail.com>
2021-11-07 20:25:16 +0300

    icons added

---
+++ src/icons/index.js
@@ -0,0 +1,48 @@
+import { ReactComponent as Star } from "./star.svg";
+import { ReactComponent as Cancel } from "./cancel.svg";
+import { ReactComponent as Person } from "./person.svg";
+import { ReactComponent as Rating } from "./rating.svg";
+import { ReactComponent as Tick } from "./tick.svg";
+import { ReactComponent as Email } from "./email.svg";
+import { ReactComponent as FacebookFilled } from "./facebook-filled.svg";
+import { ReactComponent as Phone } from "./phone.svg";
+import { ReactComponent as TickEmpty } from "./tick-empty.svg";
+import { ReactComponent as Payment } from "./payment.svg";
+import { ReactComponent as Facebook } from "./facebook.svg";
+import { ReactComponent as Twitter } from "./twitter.svg";
+import { ReactComponent as Linkedin } from "./linkedin.svg";
+import { ReactComponent as Url } from "./url.svg";
+import { ReactComponent as Reliability } from "./reliability.svg";
+import { ReactComponent as Timing } from "./timing.svg";
+import { ReactComponent as Communication } from "./communication.svg";
+import { ReactComponent as ArrowBottom } from "./arrow-bottom.svg";
+import { ReactComponent as Search } from "./search.svg";
+import { ReactComponent as Plus } from "./plus.svg";
+import { ReactComponent as Message } from "./message.svg";
+import { ReactComponent as GooglePlay } from "./googleplay.svg";
+import { ReactComponent as Appstore } from "./appstore.svg";
+export {
+  Star,
+  Cancel,
+  Person,
+  Rating,
+  Tick,
+  Email,
+  FacebookFilled,
+  Phone,
+  TickEmpty,
+  Payment,
+  Facebook,
+  Twitter,
+  Linkedin,
+  Url,
+  Reliability,
+  Timing,
+  Communication,
+  ArrowBottom,
+  Search,
+  Plus,
+  Message,
+  Appstore,
+  GooglePlay,
+};
