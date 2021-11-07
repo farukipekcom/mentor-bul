f14886f93a2e4e00eaf5d6c758719b1136e982cf
Faruk İpek <farukipekweb@gmail.com>
2021-11-07 20:34:40 +0300

    page components added

---
+++ src/pages/components/index.js
@@ -0,0 +1,6 @@
+import Header from "./Header/Header";
+import Category from "./Category/Category";
+import Sidebar from "./Sidebar/Sidebar";
+import Footer from "./Footer/Footer";
+
+export { Header, Category, Sidebar, Footer };
