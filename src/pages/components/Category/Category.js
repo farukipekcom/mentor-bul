bf2a58258567daf40ae26a0d6f7a2e41bf8145d1
Faruk İpek <farukipekweb@gmail.com>
2021-11-07 20:33:23 +0300

    Category added

---
+++ src/pages/components/Category/Category.js
@@ -0,0 +1,35 @@
+import "./Category.scss";
+function Category() {
+  return (
+    <div className="category">
+      <div className="category-list">
+        <div className="category-list-item border-animation menu-border">
+          Grafik & Tasarım
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          İnternet Reklamcılığı
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          Yazı & Çeviri
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          Video & Animasyon
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          Müzik
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          Eğlence
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          Yazılım & Teknoloji
+        </div>
+        <div className="category-list-item border-animation menu-border">
+          İş & Yönetim
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Category;
