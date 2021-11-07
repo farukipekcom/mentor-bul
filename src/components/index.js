2f046ccfc8bc67690d2ef93216510dabf7b30d35
Faruk İpek <farukipekweb@gmail.com>
2021-11-07 20:32:31 +0300

    some components added

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import AboutCard from "./AboutCard/AboutCard";
+import ProfessionCard from "./ProfessionCard/ProfessionCard";
+import ProfileCard from "./ProfileCard/ProfileCard";
+import ProjectSquareCard from "./ProjectSquareCard/ProjectSquareCard";
+import ReviewsCard from "./ReviewsCard/ReviewsCard";
+import ShareCard from "./ShareCard/ShareCard";
+import Stars from "./Stars/Stars";
+import StaticsCard from "./StaticsCard/StaticsCard";
+import VerificationCard from "./VerificationCard/VerificationCard";
+
+export {
+  AboutCard,
+  ProfessionCard,
+  ProfileCard,
+  ProjectSquareCard,
+  ReviewsCard,
+  ShareCard,
+  Stars,
+  StaticsCard,
+  VerificationCard,
+};
