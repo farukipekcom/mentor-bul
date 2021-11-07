df69d22817de599e912abdbf2dfebc98ca1aaffa
Faruk İpek <farukipekweb@gmail.com>
2021-11-07 20:25:55 +0300

    images added

---
+++ src/images/index.js
@@ -0,0 +1,6 @@
+import profilePhoto from "./profile.jpg";
+import user1 from "./user1.jpg";
+import user2 from "./user2.jpg";
+import user3 from "./user3.jpg";
+import user4 from "./user4.jpg";
+export { profilePhoto, user1, user2, user3, user4 };
