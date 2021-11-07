f2a1b6dd33a3a190395008313d3f88167fe0d1e9
Faruk İpek <farukipekweb@gmail.com>
2021-11-07 20:27:23 +0300

    aboutcard added

---
+++ src/components/AboutCard/AboutCard.js
@@ -0,0 +1,17 @@
+import "./AboutCard.scss";
+function AboutCard() {
+  return (
+    <div className="aboutCard">
+      <h3 className="aboutCard-heading">Hakkımda</h3>
+      <p className="aboutCard-text">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequat duis enim velit mollit. Exercitation
+        veniam consequat sunt nostrud amet. Amet minim mollit non deserunt
+        ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis
+        enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+      </p>
+    </div>
+  );
+}
+
+export default AboutCard;
