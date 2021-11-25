b5c9d6cb2d9c36fcc8c762e76e038711a83b7a6c
Faruk İpek <farukipekweb@gmail.com>
2021-11-25 10:51:56 +0300

    header and category menu is now responsive

--- src/pages/components/Category/Category.js
+++ src/pages/components/Category/Category.js
@@ -1,34 +1,66 @@
 import "./Category.scss";
+import { slide as Menu } from "react-burger-menu";
+
 function Category() {
   return (
-    <div className="category">
-      <div className="category-list">
-        <div className="category-list-item border-animation menu-border">
-          <a href="/category">Grafik & Tasarım</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/category">İnternet Reklamcılığı</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/category">Yazı & Çeviri</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/category">Video & Animasyon</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/sub-category">Müzik</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/sub-category">Eğlence</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/sub-category">Yazılım & Teknoloji</a>
-        </div>
-        <div className="category-list-item border-animation menu-border">
-          <a href="/sub-category">İş & Yönetim</a>
+    <>
+      <div className="category category-desktop">
+        <div className="category-list">
+          <div className="category-list-item border-animation menu-border">
+            <a href="/category">Grafik & Tasarım</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/category">İnternet Reklamcılığı</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/category">Yazı & Çeviri</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/category">Video & Animasyon</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/sub-category">Müzik</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/sub-category">Eğlence</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/sub-category">Yazılım & Teknoloji</a>
+          </div>
+          <div className="category-list-item border-animation menu-border">
+            <a href="/sub-category">İş & Yönetim</a>
+          </div>
         </div>
       </div>
-    </div>
+      <div className="category-mobile">
+        <Menu right>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/category">Grafik & Tasarım</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/category">İnternet Reklamcılığı</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/category">Yazı & Çeviri</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/category">Video & Animasyon</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/sub-category">Müzik</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/sub-category">Eğlence</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/sub-category">Yazılım & Teknoloji</a>
+          </div>
+          <div className="category-list-item border-animation menu-border menu-item">
+            <a href="/sub-category">İş & Yönetim</a>
+          </div>
+        </Menu>
+      </div>
+    </>
   );
 }
 
