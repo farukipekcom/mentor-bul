73841bf2e0d8f96b2c513d3ea6d3688ca7c50417
Faruk İpek <farukipekweb@gmail.com>
2021-11-27 11:28:20 +0300

    education: responsive 🎉

--- src/pages/SettingEducation/SettingEducation.js
+++ src/pages/SettingEducation/SettingEducation.js
@@ -1,10 +1,10 @@
 import "./SettingEducation.scss";
 import { Header, Category, Footer } from "../components";
 import { SettingsMenuCard, SettingsEducationCard } from "../../components";
-
+import { slide as Menu } from "react-burger-menu";
 function SettingEducation() {
   return (
-    <div className="rightSidebar">
+    <div className="main">
       <Header />
       <Category />
       <div className="container">
@@ -12,6 +12,11 @@ function SettingEducation() {
           <SettingsMenuCard />
         </div>
         <div className="content">
+          <div className="navLeftMenu">
+            <Menu burgerButtonClassName={"leftMenu"}>
+              <SettingsMenuCard />
+            </Menu>
+          </div>
           <div className="title">Eğitim Bilgileri</div>
           <SettingsEducationCard />
         </div>
