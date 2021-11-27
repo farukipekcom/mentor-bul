e168a6c4b9df6402a6622ccbfe8368a654d72737
Faruk İpek <farukipekweb@gmail.com>
2021-11-27 15:35:42 +0300

    menteeprojects: responsive 🎉

--- src/pages/MenteeProjects/MenteeProjects.js
+++ src/pages/MenteeProjects/MenteeProjects.js
@@ -1,9 +1,10 @@
 import "./MenteeProjects.scss";
 import { Header, Category, Footer } from "../../pages/components";
 import { ProfileMenuCard, ProjectCardHorizontal } from "../../components/";
+import { slide as Menu } from "react-burger-menu";
 function MenteeProjects() {
   return (
-    <div className="rightSidebar">
+    <div className="main">
       <Header />
       <Category />
       <div className="container">
@@ -11,13 +12,20 @@ function MenteeProjects() {
           <ProfileMenuCard />
         </div>
         <div className="content">
+          <div className="navLeftMenu">
+            <Menu burgerButtonClassName={"leftMenu"}>
+              <ProfileMenuCard />
+            </Menu>
+          </div>
           <span className="heading">Mentör’lerden almış olduğum hizmetler</span>
-          <ProjectCardHorizontal active="1" />
-          <ProjectCardHorizontal active="1" />
-          <ProjectCardHorizontal active="1" />
-          <ProjectCardHorizontal active="1" />
-          <ProjectCardHorizontal active="1" />
-          <ProjectCardHorizontal active="1" />
+          <div className="list">
+            <ProjectCardHorizontal active="1" />
+            <ProjectCardHorizontal active="1" />
+            <ProjectCardHorizontal active="1" />
+            <ProjectCardHorizontal active="1" />
+            <ProjectCardHorizontal active="1" />
+            <ProjectCardHorizontal active="1" />
+          </div>
         </div>
       </div>
       <Footer />
